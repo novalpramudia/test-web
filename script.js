163e5db4840f8baf18f38b2e9c7f12f0d5e498d6
Noval Pramudia <novalpramudia102@gmail.com>
2026-01-11 16:00:18 +0700

    first portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-function ubahText() {
-  document.getElementById("text").innerText = "✅ Teks berhasil diubah pakai JavaScript!";
-}
-
-function gantiWarna() {
-  const colors = ["#0a0f1c", "#1c0a1c", "#0a1c10", "#1c1c0a", "#001f3f"];
-  const random = Math.floor(Math.random() * colors.length);
-  document.body.style.background = colors[random];
-}
